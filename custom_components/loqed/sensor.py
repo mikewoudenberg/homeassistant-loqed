c7b4a4b756eac287f945e90bbdb65996dc46c065
Mike Woudenberg <mwoudenberg@xebia.com>
2023-01-16 21:48:13 +0100

    Adds sensors for more insights

--- custom_components/loqed/sensor.py
+++ custom_components/loqed/sensor.py
@@ -1,93 +1,74 @@
-"""Loqed sensor entities."""
-from __future__ import annotations
+"""Creates LOQED sensors."""
+from typing import Final
+
+from .coordinator import LoqedDataCoordinator, StatusMessage
+from .entity import LoqedEntity
 
 from homeassistant.components.sensor import (
-    SensorDeviceClass,
     SensorEntity,
     SensorEntityDescription,
     SensorStateClass,
+    SensorDeviceClass,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import PERCENTAGE, SIGNAL_STRENGTH_DECIBELS
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.entity import DeviceInfo, EntityCategory
+from homeassistant.const import PERCENTAGE, SIGNAL_STRENGTH_DECIBELS_MILLIWATT
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from . import LoqedDataCoordinator
-from .const import CONF_COORDINATOR, CONF_LOCK, DOMAIN
+from .const import DOMAIN
 
-SENSORS: list[SensorEntityDescription] = [
+SENSORS: Final[tuple[SensorEntityDescription, ...]] = (
     SensorEntityDescription(
-        name="Loqed battery status",
-        key="battery_percentage",
-        device_class=SensorDeviceClass.BATTERY,
-        native_unit_of_measurement=PERCENTAGE,
-        icon="mdi:battery",
-    ),
-    SensorEntityDescription(
-        name="Loqed wifi signal strength",
-        key="wifi_strength",
-        device_class=SensorDeviceClass.SIGNAL_STRENGTH,
-        native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS,
-        entity_registry_enabled_default=False,
-        icon="mdi:signal",
-    ),
-    SensorEntityDescription(
-        name="Loqed bluetooth signal strength",
         key="ble_strength",
+        name="Bluetooth signal",
         device_class=SensorDeviceClass.SIGNAL_STRENGTH,
-        native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS,
+        native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+        state_class=SensorStateClass.MEASUREMENT,
+        entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
-        icon="mdi:signal",
     ),
-]
+    SensorEntityDescription(
+        key="battery_percentage",
+        name="Battery strength",
+        device_class=SensorDeviceClass.BATTERY,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+)
 
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Set up the Loqed sensor."""
-    coordinator: LoqedDataCoordinator = hass.data[DOMAIN][entry.entry_id][
-        CONF_COORDINATOR
-    ]
-    mac_address = hass.data[DOMAIN][entry.entry_id][CONF_LOCK].id
+    """Set up the Loqed lock platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
 
-    entities = [LoqedSensor(mac_address, sensor, coordinator) for sensor in SENSORS]
-    async_add_entities(entities)
+    async_add_entities(LoqedSensor(coordinator, sensor, entry) for sensor in SENSORS)
 
 
-class LoqedSensor(CoordinatorEntity[LoqedDataCoordinator], SensorEntity):
-    """Class representing a LoqedSensor."""
-
-    _attr_state_class = SensorStateClass.MEASUREMENT
-    _attr_entity_category = EntityCategory.DIAGNOSTIC
+class LoqedSensor(LoqedEntity, SensorEntity):
+    """Representation of Sensor state."""
 
     def __init__(
         self,
-        mac_address: str,
-        sensor_description: SensorEntityDescription,
         coordinator: LoqedDataCoordinator,
+        description: SensorEntityDescription,
+        entry: ConfigEntry,
     ) -> None:
-        """Initialize the loqed sensor."""
+        """Initialize the sensor."""
         super().__init__(coordinator)
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, mac_address)},
-            name="Loqed instance",
-        )
-        self.entity_description = sensor_description
-        self._attr_unique_id = f"{sensor_description.key}-{mac_address}"
-        self._attr_native_unit_of_measurement = (
-            sensor_description.native_unit_of_measurement
-        )
+        self.entity_description = description
+        self.entry = entry
 
-        self._attr_native_value = coordinator.data[sensor_description.key]
+        self._attr_unique_id = f"{entry.unique_id}_{description.key}"
 
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
-        key = self.entity_description.key
+    @property
+    def data(self) -> StatusMessage:
+        """Return data object from DataUpdateCoordinator."""
+        return self.coordinator.lock
 
-        if key in self.coordinator.data:
-            self._attr_native_value = self.coordinator.data[key]
-            self.async_write_ha_state()
+    @property
+    def native_value(self) -> int:
+        """Return state of sensor."""
+        return getattr(self.data, self.entity_description.key)
